5512a7b1fd0c3e05b44d507a08573b11005b966d
Sarrah Vesselov <sarchka1@gmail.com>
2015-03-09 14:32:06 -0400

    finally got back button working!

--- js/app.js
+++ js/app.js
@@ -57,12 +57,13 @@ var App = (function() {
       
       var albumGroupsList = new AlbumGroupsList(albumData);
       console.log(albumGroupsList);
-      this.$sidebar.append( albumGroupsList.render() );//transition not working
+      this.$sidebar.append( albumGroupsList.render() );
 
     },
 
     showPhotoThumbs: function(photo) { 
       var photoData = new PhotoCollectionList(photo);
+
       this.$content.html( photoData.render());
       this.$sidebar.show();
     },
@@ -75,25 +76,26 @@ var App = (function() {
       var photo = new PhotoFull(photoData);
       this.$content.html( photo.render() );
 
-      $(".album-full a").on("click", function(event){
+      /* $(".album-full a").on("click", function(event){// click "back to all albums"
         event.preventDefault();
 
-        var currentAlbumPics = photoId.album;
+        var currentAlbumPics = photoData.group;
         app.showPhotoThumbs(currentAlbumPics);
-        console.log(currentAlbumPics);
-      });
+        console.log(currentAlbumPics);// logs name if given photoData
+      });*/
 
     },
 
     addListeners: function() {
       var app = this;
 
-       this.$content.on("click", ".thumbBox a", function(e){  //supposed to allow click on thumbox to show photos
+       this.$content.on("click", ".thumbBox a", function(e){  // allow click on thumbox 
+                                                              // to show photos
         e.preventDefault();
 
         var $clickedAlbum = $(e.currentTarget);
 
-        var indAlbum = $clickedAlbum.data("album-name");//refers to albumthumb file
+        var indAlbum = $clickedAlbum.data("album-name");  //refers to albumthumb file
 
         var currentAlbumPics = _.filter(app.data, function(photo) {
           return photo.album === indAlbum;
@@ -102,7 +104,8 @@ var App = (function() {
         console.log(currentAlbumPics);
       });
 
-       this.$sidebar.on("click", "li a", function(e){ //supposed to allow click on li to show photos in that album
+       this.$sidebar.on("click", "li a", function(e){ // allow click on li to show photos 
+                                                      // in that album
         e.preventDefault();
 
         var $clickedName = $(e.currentTarget);
@@ -117,7 +120,24 @@ var App = (function() {
         app.showPhotoThumbs(currentAlbumPics);
       });
 
-       this.$content.on("click", ".photoThumb a", function(e) {
+       this.$content.on("click", ".back_button a", function(e){// click "back to all albums"
+          e.preventDefault();
+
+        var $clickedName = $(e.currentTarget);
+
+        var albumName = $clickedName.data("list-name");
+
+        var currentAlbumPics = _.filter(app.data, function(photo) {
+          return photo.album === albumName;
+
+        });
+
+          app.showPhotoThumbs(currentAlbumPics);
+          console.log(currentAlbumPics);// logs name if given photoData
+      });
+
+       this.$content.on("click", ".photoThumb a", function(e) { // allows for click on photo 
+                                                                // thumbs inside album
           e.preventDefault();
           $clicked = $(e.currentTarget);
           var photoId = $clicked.data("photo-id");
--- js/templates.js
+++ js/templates.js
@@ -29,7 +29,11 @@ this["JST"]["photocoll"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"]
 this["JST"] = this["JST"] || {};
 this["JST"]["photofull"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
-  return "\n<div class=\"album-full\">\n  <a href=\"#\">Back to Albums</a>\n  <div class=\"photo\">\n    <img src=\""
+  return "\n<div class=\"album-full\">\n  <div class=\"back_button\"><a href=\"#\" data-list-name=\""
+    + escapeExpression(((helper = (helper = helpers.album || (depth0 != null ? depth0.album : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"album","hash":{},"data":data}) : helper)))
+    + "\">Back to "
+    + escapeExpression(((helper = (helper = helpers.album || (depth0 != null ? depth0.album : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"album","hash":{},"data":data}) : helper)))
+    + "</a></div>\n  <div class=\"photo\">\n    <img src=\""
     + escapeExpression(((helper = (helper = helpers.photo_url || (depth0 != null ? depth0.photo_url : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"photo_url","hash":{},"data":data}) : helper)))
     + "\" alt=\""
     + escapeExpression(((helper = (helper = helpers.album || (depth0 != null ? depth0.album : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"album","hash":{},"data":data}) : helper)))
